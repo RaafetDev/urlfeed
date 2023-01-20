7f31a3187efca47a7b6dfb2632ffde5f8c5e891c
RaafetDev <raafet.dev@gmail.com>
2023-01-20 18:55:15 +0100

    add code update step-1

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const express = require('express');
 const feed2json = require('feed2json');
-const request = require('request')
+const request = require('request');
+const nodemailer = require('nodemailer');
+var words = require("naughty-words");
+var striptags = require('striptags');
+/*\
+============================================================================
+config
+============================================================================
+\*/
 const app = express();
-
-const getFeed = (url) => {
-    return new Promise((resolve, reject) => {
-      
-    })
-};
-async function mkFeed(arg) {
-	return await getFeed(arg.query.url);
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dzgo.email@gmail.com',
+    pass: 'xygeyxkldzmufwpq'
+  }
+});
+function validate_text(compare) {
+    var swear_words_arr = words.ar;
+    var swear_alert_arr = new Array;
+    var swear_alert_count = 0;
+    var compare_text = compare;
+    for (var i = 0; i < swear_words_arr.length; i++) {
+        for (var j = 0; j < (compare_text.length); j++) {
+            if (swear_words_arr[i] == compare_text.substring(j, (j + swear_words_arr[i].length)).toLowerCase()) {
+                swear_alert_arr[swear_alert_count] = compare_text.substring(j, (j + swear_words_arr[i].length));
+                swear_alert_count++;
+            }
+        }
+    }
+    var alert_text = "";
+    for (var k = 1; k <= swear_alert_count; k++) {
+        alert_text += "(" + k + ")  " + swear_alert_arr[k - 1];
+        if (k < swear_alert_count) {
+            alert_text += ", ";
+        }
+    }
+    if (swear_alert_count > 0) {
+        return {valid:false,err:alert_text};
+    } else {
+        return {valid:true};
+    }
 }
+/*\
+============================================================================
+middleware
+============================================================================
+\*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/*\
+============================================================================
+request
+============================================================================
+\*/
+/* rss feed to json */
 app.get('/', (req, res) => {
 	if (req.query.url) {
 		var url = req.query.url;
@@ -39,7 +84,39 @@ app.get('/', (req, res) => {
 	}
 	
 });
-
-
+/* add new post */
+app.post('/addPost', (req, res) => {
+	
+	if (req.body.id && req.body.titel && req.body.post) {
+		var UID = req.body.id;
+		var titel = striptags(req.body.titel);
+		var post = req.body.post;
+		var check = validate_text(titel+" "+post);
+		if (check.valid) {
+			const mailOptions = {
+			  from: 'dzgo.email@gmail.com',
+			  to: 'dzgo.email.system@blogger.com',
+			  subject: titel,
+			  html: post+'<hidden style="display:none;">UID:'+UID+':</hidden>'
+			};
+			transporter.sendMail(mailOptions, function(error, info){
+			  if (error) {
+			 	res.json({status: false, error: error});
+			  } else {
+			    res.json({status: true, msg: 'New Post add successful'});
+			  }
+			});
+		} else {
+			res.json({status: false, valid: false, error: check.err});
+		}
+	} else {
+		res.json({status: false, error: 'required parameter not found'});
+	}
+});
+/*\
+============================================================================
+start server
+============================================================================
+\*/
 app.listen(process.env.PORT || 3000);
 module.exports = app
