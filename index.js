d0ecb368a998312b301a21c200c410829387e3d8
RaafetDev <raafet.dev@gmail.com>
2023-01-29 01:29:27 +0100

    add code update step-3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const feed2json = require('feed2json');
 const request = require('request');
-const nodemailer = require('nodemailer');
+const sendmail = require('sendmail')({silent: true});
 var words = require("naughty-words");
 var striptags = require('striptags');
 /*\
@@ -10,13 +10,7 @@ config
 ============================================================================
 \*/
 const app = express();
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.smb_user,
-    pass: process.env.smb_pass
-  }
-});
+
 function validate_text(compare) {
     var swear_words_arr = words.ar;
     var swear_alert_arr = new Array;
@@ -55,7 +49,9 @@ app.use(express.urlencoded({ extended: true }));
 request
 ============================================================================
 \*/
+/*========================================================================*/
 /* rss feed to json */
+/*========================================================================*/
 app.get('/', (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	if (req.query.url) {
@@ -85,26 +81,26 @@ app.get('/', (req, res) => {
 	}
 	
 });
+/*========================================================================*/
 /* add new post */
+/*========================================================================*/
 app.post('/addPost', (req, res) => {
-	
 	if (req.body.id && req.body.titel && req.body.post) {
 		var UID = req.body.id;
 		var titel = striptags(req.body.titel);
 		var post = req.body.post;
 		var check = validate_text(titel+" "+post);
 		if (check.valid) {
-			const mailOptions = {
-			  from: process.env.smb_user,
+			sendmail({
+			  from: 'dzgo@dzgo.com',
 			  to: process.env.blogger_email,
 			  subject: titel,
 			  html: post+'<hidden style="display:none;">UID:'+UID+':</hidden>'
-			};
-			transporter.sendMail(mailOptions, function(error, info){
-			  if (error) {
-			 	res.json({status: false, error: error});
+			}, function (err, reply) {
+			  if (err) {
+			    res.json({status: false, valid: false, error: err});
 			  } else {
-			    res.json({status: true, msg: 'New Post add successful'});
+			    res.json({status: true, msg: 'New Storie add successful'});
 			  }
 			});
 		} else {
@@ -115,10 +111,34 @@ app.post('/addPost', (req, res) => {
 	}
 });
 /*========================================================================*/
-/*
-664fbff227b1b93d24b41d0321f2166c7dbaa60e
-DBauth
-*/
+/* add new storie */
+/*========================================================================*/
+app.post('/addPost', (req, res) => {
+	if (req.body.UID && req.body.type && req.body.url) {
+		var data = {
+		    UID: req.body.UID,
+		    type: req.body.type,
+		    url: req.body.url,
+		    text: req.body.type || false
+		};
+		var post = JSON.stringify(data);
+		sendmail({
+		  from: 'dzgo@dzgo.com',
+		  to: process.env.storie_url,
+		  subject: req.body.UID,
+		  html: post
+		}, function (err, reply) {
+		  if (err) {
+		    res.json({status: false, valid: false, error: err});
+		  } else {
+		    res.json({status: true, msg: 'New Storie add successful'});
+		  }
+		});
+	} else {
+		res.json({status: false, error: 'required parameter not found'});
+	}
+});
+
 /*\
 ============================================================================
 start server
@@ -126,52 +146,3 @@ start server
 \*/
 app.listen(process.env.PORT || 3000);
 module.exports = app
-
-/*
-var settings = {
-  "url": "https://cloud.seatable.io/api/v2.1/dtable/app-access-token/",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
-    "Accept": "application/json; charset=utf-8; indent=4",
-    "Authorization": "Token d4c25e8b1af188eufh3741a3404f635349cebb46"
-  },
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
-
-
-
-
-{
-    "app_name": "DBauth",
-    "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzQ2MDA5MTYsImR0YWJsZV91dWlkIjoiNWMyNjUzNDMtOGIyNC00NmY2LWEyYTYtY2QwODNkNjU2ZTY4IiwidXNlcm5hbWUiOiIiLCJwZXJtaXNzaW9uIjoicnciLCJhcHBfbmFtZSI6IkRCYXV0aCJ9.HrpmeXIw3LHBrJKkWFQISxTuCIMLPp60086LKPdakfw",
-    "dtable_uuid": "5c265343-8b24-46f6-a2a6-cd083d656e68",
-    "dtable_server": "https://cloud.seatable.io/dtable-server/",
-    "dtable_socket": "https://cloud.seatable.io/",
-    "dtable_db": "https://cloud.seatable.io/dtable-db/",
-    "workspace_id": 30283,
-    "dtable_name": "databass"
-}
-
-
-
-{
-    "app_name": "DBauth",
-    "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzQ2MDEwMDEsImR0YWJsZV91dWlkIjoiNWMyNjUzNDMtOGIyNC00NmY2LWEyYTYtY2QwODNkNjU2ZTY4IiwidXNlcm5hbWUiOiIiLCJwZXJtaXNzaW9uIjoicnciLCJhcHBfbmFtZSI6IkRCYXV0aCJ9.zZXznCvIeVnRRyy3YeIe-gkRJCyGWVoSDmo0eZrd5mg",
-    "dtable_uuid": "5c265343-8b24-46f6-a2a6-cd083d656e68",
-    "dtable_server": "https://cloud.seatable.io/dtable-server/",
-    "dtable_socket": "https://cloud.seatable.io/",
-    "dtable_db": "https://cloud.seatable.io/dtable-db/",
-    "workspace_id": 30283,
-    "dtable_name": "databass"
-}
-
-
-
-
-
-*/
