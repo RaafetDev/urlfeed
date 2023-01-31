d8b9055b74e3e811eaee494618bb4476bf460ada
RaafetDev <121636624+RaafetDev@users.noreply.github.com>
2023-01-31 02:39:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 /*\
